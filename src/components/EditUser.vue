5a954393afd8e540304c962d08ac4ccf0be35711
Julian <61467871+JLN-BIT@users.noreply.github.com>
2022-06-21 09:04:16 +0200

    bugfixes

--- src/components/EditUser.vue
+++ src/components/EditUser.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="vue-template">
-    <form id="editTicket" action="" method="POST"> <!-- Action URL -->
+    <form id="editUser" action="" method="POST"> <!-- Action URL -->
       <h3>Edit User</h3>
 
       <div class="form-group">
         <label>User ID</label>
-        <input type="text" class="form-control form-control-lg" id="User ID" required/>
+        <input type="text" class="form-control form-control-lg" id="userid" required/>
       </div>
 
       <div class="form-group">
         <label>Name</label>
-        <input type="text" class="form-control form-control-lg" id="User Name" required/>
+        <input type="text" class="form-control form-control-lg" id="username" required/>
       </div>
 
      <div class="form-group">
@@ -19,8 +19,13 @@
       </div>
 
        <div class="form-group">
-        <label> Berechtigung</label>
-        <input type="text" class="form-control form-control-lg" id="Berechtigung" required />
+        <label>Permission</label>
+        <select class="form-control form-control-lg" required >
+            <option selected disabled value="">Please select a User Level</option>
+            <option value="0">0 - Not Approved User</option>
+            <option value="1">1 - User</option>
+            <option value="2">2 - Administrator</option>
+        </select>
       </div>
 
       <button type="submit" class="btn btn-dark btn-lg btn-block">
@@ -29,6 +34,42 @@
     </form>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+
+      return {
+        currentDate: '2022-05-05'
+    };
+  },
+methods:{
+  responseReact(){
+      document.forms["editUser"].addEventListener("submit", async (event) => {
+        event.preventDefault();
+        const resp = await fetch(event.target.action, {
+          method: "POST",
+          body: new URLSearchParams(new FormData(event.target)),
+        });
+        const body = await resp.json();
+        if(response.status==200){
+          alert("Authentification was successful"); //To Remove after implemetation of "Home" Page
+          //location.href=""; //TODO: Page to redirect to - if server does not automatically redirect
+        }else{
+          alert("Authentification failed");
+        }
+        
+      });
+  }
+  }
+  ,
+  created: function(){
+        this.currentDate = this.date_function()
+      }
+
+};
+</script>
+
 
 <style scoped>
 .inner-block {
@@ -41,5 +82,7 @@ textarea {
     border: 1px solid #ced4da;
 
 }
-
+.btn-lg {
+    margin-top: 1vh;
+}
 </style>
